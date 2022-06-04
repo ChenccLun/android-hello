cdda0f78637ed4d3cb8ac6f59c322c4a72990e12
Kiryu <956008285@qq.com>
2022-06-04 23:36:48 +0800

    v0.5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mIsbnEt = null;
+    private Button mSearchBtn = null;
+    private TextView mIsbnTxv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_addbook);
+
+        //get component
+        mIsbnEt =findViewById(R.id.editText);
+        mSearchBtn=findViewById(R.id.button);
+        mIsbnTxv = findViewById(R.id.receivedView);
+
+        //bind event
+        mSearchBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //get isbn
+                String isbn=mIsbnEt.getText().toString();
+                // search
+                //display
+                mIsbnTxv.setText(isbn);
+            }
+        });
     }
 }
